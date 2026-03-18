7a690e0f0e0843e93daa9e7b49b12dcb41533279
Leonardo-marques-moreira <lm.moreira04@gmail.com>
2026-03-18 20:25:58 -0300

    https://github.com/Leonardo-marques-moreira/ProjetoQueUtilizaMiniFramework.git

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ import br.unipar.framework.miniframework.Dispatcher;
 import br.unipar.framework.miniframework.Request;
 import br.unipar.framework.miniframework.Response;
 import br.unipar.frameworks.commands.HelloCommand;
+import br.unipar.frameworks.commands.MultiplicacaoCommand;
 import br.unipar.frameworks.commands.SomaCommand;
+import br.unipar.frameworks.commands.SubtracaoCommand;
 
 import java.util.Scanner;
 
@@ -13,11 +15,13 @@ public class Main {
 
         dispatcher.register("hello", new HelloCommand());
         dispatcher.register("soma", new SomaCommand());
+        dispatcher.register("subtracao", new SubtracaoCommand());
+        dispatcher.register("multiplicacao", new MultiplicacaoCommand());
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("App iniciado! Digite um comando:" +
-                " (Ex: 'hello' ou 'soma') ou 'sair' para sair");
+                " (Ex: 'hello', 'soma', 'subtracao' ou 'multiplicacao') ou 'sair' para sair");
 
         while (true) {
             System.out.print("> ");
